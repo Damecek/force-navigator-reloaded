80e8b31dcbd3ba640a3c4d179232e283cbb26122
Adam Stepanek <stepanek.ada@seznam.cz>
2025-05-14 21:12:18 +0200

    remove pasteFromClipboard action

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,15 +1,5 @@
 import { ui, forceNavigator } from "./shared"
 
-forceNavigator.pasteFromClipboard = (newtab)=>{
-	let cb = document.createElement("textarea")
-	let body = document.getElementsByTagName('body')[0]
-	body.appendChild(cb)
-	cb.select()
-	document.execCommand('paste')
-	const clipboardValue = cb.value.trim()
-	cb.remove()
-	return clipboardValue
-}
 forceNavigator.getIdFromUrl = ()=>{
 	const url = document.location.href
 	const ID_RE = [
