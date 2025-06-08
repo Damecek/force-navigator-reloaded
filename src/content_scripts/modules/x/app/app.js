75e92fe407588dbd1c16c62178d2c04cb540cb4c
Adam Stepanek <stepanek.ada@seznam.cz>
2025-06-08 11:38:14 +0200

    add logging to app component

--- src/content_scripts/modules/x/app/app.js
+++ src/content_scripts/modules/x/app/app.js
@@ -21,6 +21,7 @@ export default class App extends LightningElement {
     if (request.action !== 'sendCommands') {
       return;
     }
+    console.log('handle commands', request);
     if (request?.data?.commands) {
       this.commands = Object.entries(request.data.commands)
         .flatMap(([className, rawArray]) =>
@@ -35,6 +36,7 @@ export default class App extends LightningElement {
     if (request.action !== 'toggleCommandPalette') {
       return;
     }
+    console.log('toggle command palette');
     this.isCommandPaletteVisible = !this.isCommandPaletteVisible;
     return false;
   };
