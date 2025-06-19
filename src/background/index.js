7e2c06614313f9c30f0fc61f33f3e7c837cc3f46
Adam Stepanek <stepanek.ada@seznam.cz>
2025-06-19 21:14:01 +0200

    chore: cleanup background index.js

--- src/background/index.js
+++ src/background/index.js
@@ -1,26 +1,34 @@
-import { handleCommand } from './listeners/commandListener.js';
 import {
   Channel,
   CHANNEL_COMPLETED_AUTH_FLOW,
   CHANNEL_GET_COMMANDS,
   CHANNEL_INVOKE_AUTH_FLOW,
   CHANNEL_SEND_COMMANDS,
+  CHANNEL_TOGGLE_COMMAND_PALETTE,
+  isContentScriptAllowedDomain,
 } from '../shared';
 import { getCommands } from './commandRegister';
-import { interactiveLogin } from './auth/auth'; /**
- * Background script entry point: commands, and message listeners.
- * @returns {void}
- */
+import { interactiveLogin } from './auth/auth';
+
+chrome.commands.onCommand.addListener(function (command, tab) {
+  const url = tab?.url;
+  if (!url || !isContentScriptAllowedDomain(url)) {
+    console.log(`handleCommand: ignored command "${command}" on URL: ${url}`);
+    return;
+  }
+  console.log('Received command:', command);
+  switch (command) {
+    case 'toggle-command-palette':
+      return new Channel(CHANNEL_TOGGLE_COMMAND_PALETTE).publish({
+        tabId: tab.id,
+      });
+    default:
+      console.error('Unknown command:', command);
+  }
+});
 
 /**
- * Background script entry point: commands, and message listeners.
- * @returns {void}
- */
-
-chrome.commands.onCommand.addListener(handleCommand);
-
-/**
- * Sending commands back through different channel as the publisher of CHANNEL_GET_COMMANDS does not need to be the one needing the commands.
+ * Sending commands back through different channel as the publisher of CHANNEL_GET_COMMANDS does not need to be the handler of the commands.
  */
 new Channel(CHANNEL_GET_COMMANDS).subscribe(async function ({ sender }) {
   const hostname = getSenderHostname(sender);
