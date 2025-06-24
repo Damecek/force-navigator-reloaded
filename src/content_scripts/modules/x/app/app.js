f0b320781f934cb3006428c2f3b3719ce5882aff
Adam Stepanek <stepanek.ada@seznam.cz>
2025-06-24 15:11:22 +0200

    chore: remove disconnected callback from x-app as the component remains for the page lifecycle

--- src/content_scripts/modules/x/app/app.js
+++ src/content_scripts/modules/x/app/app.js
@@ -10,51 +10,32 @@ import {
 
 /**
  * App component for the command palette.
- * It is responsible for rendering the command palette and handling the commands.
+ * Renders the palette UI and manages command subscriptions.
  *
- * @property {Array<Command>} commands - Array of commands to be rendered in the command palette.
- * @property {boolean} isCommandPaletteVisible - Flag to indicate whether the command palette is visible or not.
- * @property {Channel} sendCommandsChannel - Channel to send commands to the background script.
- * @property {Channel} toggleCommandPaletChannel - Channel to toggle the visibility of the command palette.
- * @property {Channel} authChannel - Channel to handle the authentication flow.
+ * @property {Array<Command>} commands - Commands available in the palette.
+ * @property {boolean} isCommandPaletteVisible - Whether the palette is visible.
  */
 export default class App extends LightningElement {
   static renderMode = 'light';
   @track commands = [];
   isCommandPaletteVisible = false;
-  sendCommandsChannel;
-  toggleCommandPaletChannel;
-  authChannel;
 
+  /**
+   * Subscribe to background channels and set up event listeners.
+   * No explicit cleanup required as the component remains for the page lifecycle.
+   */
   connectedCallback() {
-    this.sendCommandsChannel = new Channel(CHANNEL_SEND_COMMANDS);
-    this.toggleCommandPaletChannel = new Channel(
-      CHANNEL_TOGGLE_COMMAND_PALETTE
+    new Channel(CHANNEL_SEND_COMMANDS).subscribe(this._handleCommands);
+    new Channel(CHANNEL_TOGGLE_COMMAND_PALETTE).subscribe(
+      this._handleToggleCommandPalette
     );
-    this.authChannel = new Channel(CHANNEL_COMPLETED_AUTH_FLOW);
-
+    new Channel(CHANNEL_COMPLETED_AUTH_FLOW).subscribe(this._handleAuth);
     window.addEventListener('keydown', this._handleEscape);
-
-    this.sendCommandsChannel.subscribe(this._handleCommands);
-    this.toggleCommandPaletChannel.subscribe(this._handleToggleCommandPalette);
-    this.authChannel.subscribe(this._handleAuth);
-
     this.publishRefreshCommands();
   }
 
   publishRefreshCommands() {
     return new Channel(CHANNEL_REFRESH_COMMANDS).publish();
-  }
-
-  // todo: needs disconnectedCallback? what are the use cases?
-  disconnectedCallback() {
-    console.log('x-app disconnected');
-    this.sendCommandsChannel.unsubscribe(this._handleCommands);
-    this.toggleCommandPaletChannel.unsubscribe(
-      this._handleToggleCommandPalette
-    );
-    this.authChannel.unsubscribe(this._handleAuth);
-    window.removeEventListener('keydown', this._handleEscape);
   }
 
   _handleAuth = () => {
